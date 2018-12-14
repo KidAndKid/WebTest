2239e2d24c08c16b863052349bfd85d54ccf2f3d
KidAndKid <202513493@qq.com>
2018-12-14 16:09:05 +0800

    新增SignalR实现简单的实时消息推送

---
+++ SignalRMvc/ChatHubs/ChatHub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SignalRMvc.ChatHubs
+{
+    public class ChatHub:Hub
+    {
+        public void SendMessage(string name, string message)
+        {
+            // Clients.All.hello();
+            Clients.All.receiveMessage(name, message);
+            //用户调用客户端的函数
+        }
+    }
+}
---
+++ SignalRMvc/ChatHubs/Startup.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+[assembly: OwinStartup(typeof(SignalRMvc.ChatHubs.Startup))]
+namespace SignalRMvc.ChatHubs
+{
+    
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+            //服务器的hub注册
+        }
+    }
+}
